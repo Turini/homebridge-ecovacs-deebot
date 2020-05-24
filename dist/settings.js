fd96d603e0b05774ce144dfe0688f3b54495edbc
Glitch (homebridge-ecovacs-deebot) <none>
2020-05-24 21:48:48 +0000

    linking w glitch account

---
+++ dist/settings.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PLUGIN_NAME = exports.PLATFORM_NAME = void 0;
+exports.PLATFORM_NAME = 'HomebridgeEcovacsDeebot';
+exports.PLUGIN_NAME = 'homebridge-ecovacs-deebot';
+//# sourceMappingURL=settings.js.map
